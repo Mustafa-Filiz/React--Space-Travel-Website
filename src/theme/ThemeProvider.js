d0cdb523cad8a624d0a560a14c5f7ab79f58cda6
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-01 23:54:41 +0300

    breakpoints added

--- src/theme/ThemeProvider.js
+++ src/theme/ThemeProvider.js
@@ -4,10 +4,19 @@ import React from 'react'
 const theme = createTheme({
 	typography:{
 		fontFamily:["Barlow Condensed"]
-	}
+	},
+	breakpoints: {
+		values: {
+		  xs: 0,
+		  sm: 600,
+		  md: 900,
+		  lg: 1200,
+		  xl: 1536,
+		},
+	  },
 })
 
-function FontTheme({children}) {
+function AppThemeProvider({children}) {
 	return (
 		<ThemeProvider theme={theme}>
 			{children}
@@ -15,4 +24,4 @@ function FontTheme({children}) {
 	)
 }
 
-export default FontTheme
+export default AppThemeProvider
