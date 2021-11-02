690a55c2e3973f81b2c989e174a03e39ab58b805
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-02 14:53:47 +0300

    responsiveness added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import bgImg from '../images/background-home-desktop.jpg';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     container: {
         width: '100%',
         height: '100vh',
@@ -14,29 +14,88 @@ const useStyles = makeStyles({
         paddingTop: 40,
     },
     main: {
-		border: "2px solid red",
         width: '90%',
         height: '80vh',
-		margin: "auto",
+        margin: 'auto',
         display: 'flex',
         justifyContent: 'space-around',
         alignItems: 'center',
         color: 'white',
+        [theme.breakpoints.down("md")] : {
+            flexDirection : 'column'
+        },
     },
     content: {
-        width: '30%',
+        width: '33%',
+        textAlign : "left",
+        [theme.breakpoints.down("md")] : {
+            width : '60%'
+        },
     },
-    explore: {
-		width: 274,
-		height: 274,
-		color: "black",
-		backgroundColor : "white",
-		borderRadius: "50%",
-		display: 'flex',
+    subtitle:{
+        fontSize : "1.75rem !important",
+        color: "#D0D6F9",
+        letterSpacing: "4.725px",
+        [theme.breakpoints.down("md")] : {
+            fontSize : "1.5rem !important",
+        },
+        [theme.breakpoints.down("sm")] : {
+            fontSize : "1rem !important",
+        },
+    },
+    title:{
+        fontSize : "9.4rem !important",
+        fontFamily : "Bellefair !important",
+        [theme.breakpoints.down("lg")] : {
+            fontSize : "8rem !important",
+        },
+        [theme.breakpoints.down("sm")] : {
+            fontSize : "5rem !important",
+        },
+    },
+    text:{
+        fontSize : "1.12rem !important",
+        fontFamily : "Barlow !important",
+        color: "#D0D6F9",
+        lineHeight : "2rem",
+        [theme.breakpoints.down("md")] : {
+            fontSize : "1rem !important",
+        },
+        [theme.breakpoints.down("sm")] : {
+            fontSize : "0.9rem !important",
+        },
+    },
+    exploreContainer: {
+        width: 274,
+        height: 274,
+        color: 'black',
+        backgroundColor: 'white',
+        borderRadius: '50%',
+        display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-	},
-});
+        fontSize : "2rem",
+        [theme.breakpoints.down("md")] : {
+            width: 242,
+            height: 242,
+            fontSize : "1rem"
+        },
+        [theme.breakpoints.down("sm")] : {
+            width: 150,
+            height: 150,
+        },
+    },
+    explore : {
+        fontSize : "3.5rem !important",
+        fontFamily : "Bellefair !important",
+        [theme.breakpoints.down("md")] : {
+            fontSize : "3rem !important"
+        },
+        [theme.breakpoints.down("sm")] : {
+            fontSize : "2rem !important"
+        },
+    },
+}));
 
 function Home() {
     const classes = useStyles();
@@ -45,17 +104,19 @@ function Home() {
             <Navbar />
             <Box className={classes.main}>
                 <Box className={classes.content}>
-                    <Typography variant="h5">SO, YOU WANT TO TRAVEL TO</Typography>
-                    <Typography variant="h1">SPACE</Typography>
-                    <Typography variant="p">
+                    <Typography className={classes.subtitle}>
+                        SO, YOU WANT TO TRAVEL TO
+                    </Typography>
+                    <Typography  className={classes.title}>SPACE</Typography>
+                    <Typography  className={classes.text}>
                         Let’s face it; if you want to go to space, you might as
                         well genuinely go to outer space and not hover kind of
                         on the edge of it. Well sit back, and relax because
                         we’ll give you a truly out of this world experience!
                     </Typography>
                 </Box>
-                <Box className={classes.explore}>
-                    <Typography variant="h2">EXPLORE</Typography>
+                <Box className={classes.exploreContainer}>
+                    <Typography className={classes.explore}>EXPLORE</Typography>
                 </Box>
             </Box>
         </Box>
