29572463a0398dbb068650a6f233899c4e15056b
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-02 14:53:20 +0300

    breakpoints removed

--- src/theme/ThemeProvider.js
+++ src/theme/ThemeProvider.js
@@ -5,15 +5,6 @@ const theme = createTheme({
 	typography:{
 		fontFamily:["Barlow Condensed"]
 	},
-	breakpoints: {
-		values: {
-		  xs: 0,
-		  sm: 600,
-		  md: 900,
-		  lg: 1200,
-		  xl: 1536,
-		},
-	  },
 })
 
 function AppThemeProvider({children}) {
