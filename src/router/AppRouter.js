4a2977868d97b577bfdfa383f7ddcfd4033f5f9d
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-10 15:41:50 +0300

    crew and tech comp added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Crew from '../pages/Crew';
 import Destination from '../pages/Destination';
 import Home from '../pages/Home';
+import Technology from '../pages/Technology';
 import AppThemeProvider from '../theme/ThemeProvider';
 
 function AppRouter() {
@@ -11,6 +13,8 @@ function AppRouter() {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/destination" component={Destination} />
+                    <Route exact path="/crew" component={Crew} />
+                    <Route exact path="/technology" component={Technology} />
                 </Switch>
             </Router>
         </AppThemeProvider>
